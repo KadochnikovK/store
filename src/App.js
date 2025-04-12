65f8d6423d5b5c2c2839ec8d1aacc134b4fda5a4
kadochnikov <kadochnikov@papillon.ru>
2025-04-12 13:35:10 +0500

    corrected links
    Signed-off-by: kadochnikov <kadochnikov@papillon.ru>

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProductsPage />} />
-        <Route path="/:id" element={<ProductDetailPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </Router>
   );
--- src/components/ProductCard/ProductCardComponent.jsx
+++ src/components/ProductCard/ProductCardComponent.jsx
@@ -3,7 +3,7 @@ import Stars from '../Stars/StarsComponent';
 
 const ProductCard = ({ product }) => {
   return (
-    <Link to={`/${product.id}`} className="product-card">
+    <Link to={`/products/${product.id}`} className="product-card">
     
       <img src={product.image} alt={product.title} />
       <div className="product-info">
